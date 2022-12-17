d4347f34f8c972f62bb773ff1f8753c45696dc18
averagephotographer <23220457+averagephotographer@users.noreply.github.com>
2022-12-17 02:44:00 -0500

    concert class

--- scraper.py
+++ scraper.py
@@ -1,4 +1,14 @@
 from urllib.request import urlopen
+import json
+
+class Concert():
+    def __init__(self, artist, is_sold_out, start_date, end_date, venue, address):
+        self.artist = artist
+        self.is_sold_out = is_sold_out
+        self.start_date = start_date
+        self.end_date = end_date
+        self.venue = venue
+        self.address = address
 
 def get_artist_id(url):
     page = urlopen(url)
@@ -18,13 +28,23 @@ def get_tour_info(artist_id):
 
     page = urlopen(api)
     html_bytes = page.read()
-    json = html_bytes.decode("utf-8")
+    tour_string = html_bytes.decode("utf-8")
+    tour_json = json.loads(tour_string)
 
-    return json
+    return tour_json
 
 url = "http://stephenday.org/tour"
 id = get_artist_id(url)
 print(id)
 
 tour_json = get_tour_info(id)
-print(tour_json)
+for i in tour_json['included']:
+    attribute = i['attributes']
+    print(json.dumps(attribute, indent=1))
+    is_sold_out = attribute['is-sold-out']
+    start_date = attribute['starts-at-date-local']
+    end_date = attribute['ends-at-date-local']
+    venue = attribute['venue-name']
+    address = attribute['formatted-address']
+
+# separate the data
