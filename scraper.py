4a38eb09cd709f2988afb8bc5223b5493e9e83fd
averagephotographer <23220457+averagephotographer@users.noreply.github.com>
2022-12-17 02:29:48 -0500

    beginning of scraper

---
+++ scraper.py
@@ -0,0 +1,47 @@
+from urllib.request import urlopen
+
+def get_artist_id(url):
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    # find 'artist-id'
+    # 11 to account for the name and the open quote
+    start = html.find('artist-id=') + 11 
+    end = start + 36
+
+    artist_id = html[start:end]
+    return artist_id
+
+def get_tour_info(artist_id):
+    api = f"https://cdn.seated.com/api/tour/{artist_id}?include=tour-events"
+
+    page = urlopen(api)
+    html_bytes = page.read()
+    json = html_bytes.decode("utf-8")
+
+    return json
+
+url = "http://stephenday.org/tour"
+id = get_artist_id(url)
+print(id)
+
+tour_json = get_tour_info(id)
+print(tour_json)
+
+"""
+
+with open("output.txt", 'w') as f:
+    f.write(html)
+
+artist_id = "205a1ad5-4413-4e72-9912-5aa8d3a6207e" # steven day
+api = f"https://cdn.seated.com/api/tour/{artist_id}?include=tour-events"
+
+
+page = urlopen(api)
+
+html_bytes = page.read()
+json = html_bytes.decode("utf-8")
+
+print(json)
+"""
